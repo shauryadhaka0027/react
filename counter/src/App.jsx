1e0af4e6fdf1d6b3b80f471a7d88928d8bda59a1
unknown <shauryadhaka3@gmail.com>
2023-10-20 18:04:36 +0530

    added code

---
+++ counter/src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import { useState } from 'react'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+      <h1>Counter</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+       
+      </div>
+     
+    </>
+  )
+}
+
+export default App
